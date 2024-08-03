b4a24be77b33558d77e4273718a57f5beb22ce06
Kavinda Dilshan <kdtechniques@gmail.com>
2024-08-04 01:39:33 +0530

    Initial Commit

---
+++ CountryCodeSheetSwiftUIUITesterApp/CountryCodeSheetSwiftUIUITesterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryCodeSheetSwiftUIUITesterAppApp.swift
+//  CountryCodeSheetSwiftUIUITesterApp
+//
+//  Created by Mr. Kavinda Dilshan on 2024-08-04.
+//
+
+import SwiftUI
+
+@main
+struct CountryCodeSheetSwiftUIUITesterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
